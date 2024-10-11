e86dc4e459715533d5cefa309ab799fad7ab8b88
Luis Fernando Farji <fernando.farji@gmail.com>
2024-10-11 10:25:17 -0300

    Nav With Toggle & Compatibility

---
+++ README.md
@@ -0,0 +1,22 @@
+# Company Website Template
+## Full Video Tutorial: https://youtu.be/HEHJmneClg0
+
+Get Full Source Code: https://www.buymeacoffee.com/cstutorials/e/102643
+
+Join this channel to see more tutorials like this responsive website! [Codingstar Tutorials](https://www.youtube.com/channel/UCadItSVcBu0pS3ES8UJQJ1A)
+
+This Website Template contains:
+<ul>
+    <li>A preloader that shows before the page completes loading</li>
+    <li>A navigation bar that is fixed at the top of the screen <br> and can direct to a section of the website. <br> It will be blurred when user scrolls down.</li>
+    <li>Swiper to swipe multiple containers</li>
+    <li>Scroll to top button that only appears when the user scrolls down <br> and will disappear when user goes back to the top</li>
+    <li>Custom scrollbar</li>
+    <li>Subscribe form design</li>
+    <li>Dark/Light mode toggle</li>
+    <li>Slide in when in view</li>
+    <li>Compatible with most devices' screens, such as mobile phones</li>
+    <li>A website favicon</li>
+<ul>
+   <br>
+<img src='website-preview.png'>
---
+++ js/main.js
@@ -0,0 +1,37 @@
+        /* ---------------- PRELOADER ---------------- */
+    
+        /* ---------------- SCROLL TO TOP BUTTON CHANGE ---------------- */
+
+        /* ---------------- CHANGE HEADER ON SCROLL ---------------- */
+        
+        /* ---------------- TOGGLE NAVIGATION ---------------- */
+       let navigation = document.querySelector('nav');
+       let navMenu = document.querySelector('.nav-menu');
+       let navToggle = document.getElementById('nav-toggle');
+       let navClose = document.getElementById('nav-close');
+
+       function showNav() {
+        navMenu.style.display = 'flex';
+        navigation.style.height = '320px';
+       }
+       
+
+       function hideNav() {
+        navMenu.style.display = 'none';
+        navigation.style.height = '80px';
+       }
+
+       
+
+        /* HIDE NAV WHEN GO TO SECTION */
+       
+        /* ---------------- SWIPER ---------------- */
+
+        /* ---------------- SUBSCRIBE FORM ---------------- */
+
+        /* ---------------- SCROLL TO ---------------- */
+        
+        /* ---------------- SCROLL REVEAL ---------------- */
+      
+        /* ---------------- TOGGLE DAY/NIGHT MODE ---------------- */
+      
