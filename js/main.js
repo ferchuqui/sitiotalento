2b2650e5c5fc4ad2ad03bc9564c0bb469a811379
Luis Fernando Farji <fernando.farji@gmail.com>
2024-10-13 03:04:32 -0300

    change header

--- js/main.js
+++ js/main.js
@@ -3,7 +3,19 @@
         /* ---------------- SCROLL TO TOP BUTTON CHANGE ---------------- */
 
         /* ---------------- CHANGE HEADER ON SCROLL ---------------- */
-        
+        let nav = document.querySelector('nav');
+
+        function changeHeader() {
+                if (document.body.scrollTop > 150) {
+                        nav.classList.add('change-header');
+                } else if (document.documentElement.scrollTop >150) {
+                
+                } 
+                else {
+                    nav.classList.remove('change-header');
+                }
+        }
+        window.addEventListener('scroll', changeHeader);
         /* ---------------- TOGGLE NAVIGATION ---------------- */
        let navigation = document.querySelector('nav');
        let navMenu = document.querySelector('.nav-menu');
@@ -23,8 +35,21 @@
 
        
 
-        /* HIDE NAV WHEN GO TO SECTION */
-       
+        /* HIDE NAV WHEN GO TO SECTION for smaller devices*/
+       let navLinks = document.querySelectorAll('.pages');
+
+       function removeNav() {
+        if ($(window).width() < 880) {
+                navMenu.style.display = 'none';
+                navigation.style.height = '80px';
+        }
+       }
+
+       navLinks.forEach(link => {
+        link.addEventListener('click', removeNav);
+       }
+        
+       )
         /* ---------------- SWIPER ---------------- */
 
         /* ---------------- SUBSCRIBE FORM ---------------- */
